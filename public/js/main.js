1bc699c478b7f7f2ffbbd7177eccf002ab3808c6
sree-86 <sriram3rhyme@gmail.com>
2019-03-01 14:44:27 -0500

    firebase login

--- public/js/main.js
+++ public/js/main.js
@@ -2,54 +2,48 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
       window.alert( "Welcome User1111111 : ");
-  
-      //document.getElementById("user_div").style.display = "block";
-      //document.getElementById("login_div").style.display = "none";
-  
+
       var user = firebase.auth().currentUser;
-  
+
       if(user != null){
         window.alert( "Welcome User : ");
         var email_id = user.email;
-        
-  
+
+
       }
-  
+
     } else {
       // No user is signed in.
       window.alert( "No User");
-      //document.getElementById("user_div").style.display = "none";
-      //document.getElementById("login_div").style.display = "block";
-  
+
     }
   });
-  
+
   function login(){
 
-    window.alert( "inside login start func : ");
 
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
 
-    
-  
+
+
     firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
         window.alert( "inside error func : ");
 
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-  
+
       window.alert("Error : " + errorMessage);
-  
+
       // ...
     });
-  
+
   }
-  
+
   function logout(){
     firebase.auth().signOut();
-  } 
+  }
 
 
 (function ($) {
@@ -102,7 +96,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    
+
+
 
 })(jQuery);

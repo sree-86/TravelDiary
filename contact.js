480b0a22ceb42893af0018b2ab702a1c7ba0a54a
sherlynlobo <sherlynlobo26@gmail.com>
2019-04-08 21:33:03 -0400

    Saved Contact page messages to firebase databade

---
+++ contact.js
@@ -0,0 +1,59 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+    apiKey: "AIzaSyAH0qvoOwlu4Vmg6s4_L6OBXHlIlirt26k",
+    authDomain: "travel-diary-mapd.firebaseapp.com",
+    databaseURL: "https://travel-diary-mapd.firebaseio.com",
+    projectId: "travel-diary-mapd",
+    storageBucket: "travel-diary-mapd.appspot.com",
+    messagingSenderId: "618580698774"
+  };
+  firebase.initializeApp(config);
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+  
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(name, company, email, phone, message);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, company, email, phone, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      company:company,
+      email:email,
+      phone:phone,
+      message:message
+    });
+  }
